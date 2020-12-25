7c712df995019a984fa238ae4aa51c50c93f12fd
gomezrondon <gomezrondon@gmail.com>
2020-12-25 08:59:25 -0600

    Implementing a exchange/queue and a controller

--- src/main/java/com/gomezrondon/springrabbitmqtest/SpringRabbitmqTestApplication.java
+++ src/main/java/com/gomezrondon/springrabbitmqtest/SpringRabbitmqTestApplication.java
@@ -1,10 +1,17 @@
 package com.gomezrondon.springrabbitmqtest;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sound.midi.Receiver;
 
 @SpringBootApplication
 public class SpringRabbitmqTestApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitmqTestApplication.class, args);
