145188800e8d013cbd642751918c83b3347d59cd
gomezrondon <gomezrondon@gmail.com>
2020-12-25 08:15:00 -0600

    1rst Commit.

---
+++ src/test/java/com/gomezrondon/springrabbitmqtest/SpringRabbitmqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezrondon.springrabbitmqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
